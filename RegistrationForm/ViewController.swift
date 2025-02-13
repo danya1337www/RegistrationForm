96257f852ad8952f4c5cc86296b11cf39cd7fe1b
Danil Chekantsev <chekantsev.danil2@gmail.com>
2025-02-13 16:05:00 +0300

    First commit. Setup UI

--- RegistrationForm/ViewController.swift
+++ RegistrationForm/ViewController.swift
@@ -7,13 +7,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var loginButton: UIButton!
+    @IBOutlet var forgotUserNameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
-
+    
+    private func updateUI() {
+        loginTextField.placeholder = "User Name"
+        passwordTextField.placeholder = "Password"
+        passwordTextField.isSecureTextEntry = true
+        
+        loginButton.setTitle("Log In", for: .normal)
+        forgotUserNameButton.setTitle("Forgot User Name?", for: .normal)
+        forgotUserNameButton.titleLabel?.font = UIFont.systemFont(ofSize: 10)
+        forgotPasswordButton.setTitle("Forgot Password?", for: .normal)
+        forgotPasswordButton.titleLabel?.font = UIFont.systemFont(ofSize: 10)
+    }
 
 }
 
